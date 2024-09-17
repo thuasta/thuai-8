b9d2850ba89c5223e8d62c8432dd076a52eecff3
Steven C <lemonkyo2222@outlook.com>
2024-09-17 17:06:08 +0800

    chore: implement LogHandler and TaskManager

--- server/src/Program.cs
+++ server/src/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Thuai.Server;
+﻿using System.Reflection;
+using Serilog;
+
+namespace Thuai.Server;
 
 public class Program
 {
+    static readonly ILogger _logger = Utility.Tools.LogHandler.CreateLogger("Program");
+
     public static void Main(string[] args)
     {
-        // TODO: Implement
-        throw new NotImplementedException();
+        try
+        {
+            Utility.Tools.LogHandler.Initialize();
+
+            Version version = Assembly.GetExecutingAssembly().GetName().Version ?? new Version(0, 0, 0, 0);
+
+            _logger.Information($"THUAI8 Server v{version}");
+            _logger.Information("Copyright (c) 2024 THUASTA");
+
+            // TODO: Implement
+
+        }
+        catch (Exception e)
+        {
+            _logger.Fatal($"Program crashed with exception:");
+            _logger.Fatal(e.Message);
+            _logger.Fatal(e.StackTrace ?? "No stack trace available.");
+        }
     }
 }
--- server/src/Utility/Tools.cs
+++ server/src/Utility/Tools.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 namespace Thuai.Server.Utility;
 
 /// <summary>
@@ -5,9 +7,44 @@ namespace Thuai.Server.Utility;
 /// </summary>
 public static class Tools
 {
+    /// <summary>
+    /// A class for handling logs.
+    /// </summary>
     public static class LogHandler
     {
-        // TODO: Implement
+        /// <summary>
+        /// Initializes logger configuration.
+        /// </summary>
+        public static void Initialize()
+        {
+            LoggerConfiguration logConfig = new();
+
+            logConfig.WriteTo.Console();
+            logConfig.MinimumLevel.Information();
+
+            Log.Logger = logConfig.CreateLogger();
+        }
+
+        /// <summary>
+        /// Creates a logger for a specific component.
+        /// </summary>
+        /// <param name="component">Name of the component.</param>
+        /// <returns>The logger.</returns>
+        public static ILogger CreateLogger(string component)
+        {
+            return Log.Logger.ForContext("Component", component);
+        }
+
+        /// <summary>
+        /// Logs an exception.
+        /// </summary>
+        /// <param name="logger">The logger that logs the exception.</param>
+        /// <param name="e">The exception.</param>
+        public static void LogException(ILogger logger, Exception e)
+        {
+            logger.Error($"An exception occurred: {e.Message}");
+            logger.Debug(e.StackTrace ?? "No stack trace available.");
+        }
     }
 
     public static class MessageHandler
@@ -15,13 +52,61 @@ public static class Tools
         // TODO: Implement
     }
 
+    /// <summary>
+    /// A class for parsing command line arguments.
+    /// </summary>
     public static class ArgParser
     {
-        // TODO: Implement
+        /// <summary>
+        /// Parses command line arguments.
+        /// </summary>
+        /// <param name="args">Argunemts from command line.</param>
+        /// <returns>Corresponding settings.</returns>
+        public static ArgSettings ParseArgs(string[] args)
+        {
+            // TODO: Implement
+            throw new NotImplementedException();
+        }
     }
 
     public static class ConfigLoader
     {
         //TODO: Implement
+    }
+
+    /// <summary>
+    /// A class for managing tasks.
+    /// </summary>
+    public static class TaskManager
+    {
+        private static int _taskId = 0;                 // The ID of the task.
+
+        /// <summary>
+        /// Creates a task.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static Task CreateTask(Action action)
+        {
+            _taskId++;
+
+            ILogger logger = LogHandler.CreateLogger($"Task {_taskId}");
+            logger.Debug("Task created.");
+
+            return new Task(
+                () =>
+                {
+                    try
+                    {
+                        action();
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error($"Task crashed:");
+                        LogHandler.LogException(logger, e);
+                    }
+                }
+            );
+        }
     }
 }
