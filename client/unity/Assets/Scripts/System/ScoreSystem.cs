67533a407051ea9aaeaf024ac3e88848f2b14ef2
forgrabbit <2133603132@qq.com>
2024-10-10 20:21:04 +0800

    Update client\unity

---
+++ client/unity/Assets/Scripts/System/ScoreSystem.cs
@@ -0,0 +1,17 @@
+using QFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BattleCity
+{
+    public class ScoreSystem : AbstractSystem
+    {
+        protected override void OnInit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+
+}
