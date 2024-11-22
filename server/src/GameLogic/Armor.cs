9ba8e9b39add8b24a5fb24e41aef1a17c9f26053
MTY2005 <ma-ty23@mails.tsinghua.edu.cn>
2024-11-22 22:49:43 +0800

    update tank

--- server/src/GameLogic/Armor.cs
+++ server/src/GameLogic/Armor.cs
@@ -1,10 +1,17 @@
 namespace Thuai.Server.GameLogic;
 
+public enum ArmorKnife{
+    NOT_OWNED,
+    AVAILABLE,
+    ACTIVE,
+    BROKEN
+}
+
 public class Armor{
     public bool canReflect;
     public int armorValue;
     public int health;
     public bool gravityField;
-    public string knife;
+    public ArmorKnife knife;
     public double dodgeRate;
 }
--- server/src/GameLogic/Skill.cs
+++ server/src/GameLogic/Skill.cs
@@ -1,7 +1,18 @@
 namespace Thuai.Server.GameLogic;
 
+public enum SkillName{
+    BLACK_OUT,
+    SPEED_UP,
+    FLASH,
+    DESTROY,
+    CONSTRUCT,
+    TRAP,
+    MISSILE,
+    KAMUI
+}
+
 public class Skill{
-    public string name;
+    public SkillName name;
     public int maxCooldown;
     public int currentCooldown;
     public bool isActive;
