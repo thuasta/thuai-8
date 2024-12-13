23eaa91b495dd032a8f39ca8ae6a0655bdb9da05
forgrabbit <2133603132@qq.com>
2024-12-13 22:47:47 +0800

    Update Skill.cs

--- server/src/GameLogic/Skill.cs
+++ server/src/GameLogic/Skill.cs
@@ -17,6 +17,8 @@ public class Skill
     public SkillName name;
     public int maxCooldown;
     public int currentCooldown;
+    public int maxActiveTime;
+    public int currentActiveTime;
 
     public Skill(SkillName skillName)
     {
@@ -28,6 +30,33 @@ public class Skill
         if (currentCooldown > 0)
         {
             currentCooldown--;
+        }
+    }
+
+    public void Start()
+    {
+
+    }
+    public void Activate()
+    {
+        currentActiveTime = maxActiveTime;
+        Start();
+    }
+
+    public void End()
+    {
+
+    }
+
+    public void UpdateActiveTime()
+    {
+        if (currentActiveTime > 0)
+        {
+            currentActiveTime--;
+            if (currentActiveTime == 0)
+            {
+                End();
+            }
         }
     }
 }
