6d9b19770c8525d120f4a881dd1f01ab6df80190
Steven C <lemonkyo2222@outlook.com>
2025-04-03 18:01:36 +0800

    upd: health related values

--- server/src/GameLogic/Constants/Constants.cs
+++ server/src/GameLogic/Constants/Constants.cs
@@ -5,11 +5,13 @@ namespace Thuai.Server.GameLogic;
 /// </summary>
 public static partial class Constants
 {
+    public const int HEALTH_VALUE_BASE = 100;
+
     //Armor
-    public const int INITIAL_ARMOR_VALUE = 0;
-    public const int ARMOR_VALUE_INCREASE = 1;
-    public const int INITIAL_HEALTH_VALUE = 3;
-    public const int REMAINING_HEALTH_VALUE = 1;
+    public const int INITIAL_ARMOR_VALUE = 0 * HEALTH_VALUE_BASE;
+    public const int ARMOR_VALUE_INCREASE = 1 * HEALTH_VALUE_BASE;
+    public const int INITIAL_HEALTH_VALUE = 3 * HEALTH_VALUE_BASE;
+    public const int REMAINING_HEALTH_VALUE = HEALTH_VALUE_BASE / 2;    // 50% of base health
     public const int INITIAL_DODGE_PERCENTAGE = 0;      // 0%
     public const int DODGE_PERCENTAGE_INCREASE = 25;    // 25%
     public const float GRAVITY_FIELD_RADIUS = 10f;
@@ -23,8 +25,8 @@ public static partial class Constants
     public const float BULLET_SPEED_INCREASE_FACTOR = 2;
     public const float INITIAL_LASER_LENGTH = INITIAL_BULLET_SPEED * LASER_LENGTH_EQUAVALENT_BULLET_FLYING_TICKS;
     public const float LASER_LENGTH_INCREASE_FACTOR = 2;
-    public const int INITIAL_DAMAGE = 1;
-    public const int DAMAGE_INCREASE = 1;
+    public const int INITIAL_DAMAGE = 1 * HEALTH_VALUE_BASE;
+    public const int DAMAGE_INCREASE = 1 * HEALTH_VALUE_BASE;
     public const int INITIAL_BULLETS = 5;
     public const int BULLETS_INCREASE = 1;
     public const int ANTI_ARMOR_FACTOR = 2;
