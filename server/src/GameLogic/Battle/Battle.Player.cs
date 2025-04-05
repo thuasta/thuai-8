c67645063fa552ff68d08bcb3f7ceb05c3290a1e
Steven C <lemonkyo2222@outlook.com>
2025-04-05 12:16:54 +0800

    upd: name of constants

--- server/src/GameLogic/Battle/Battle.Player.cs
+++ server/src/GameLogic/Battle/Battle.Player.cs
@@ -48,7 +48,9 @@ public partial class Battle
         {
             float x = Constants.WALL_LENGTH * (_random.Next(0, Map.Width) + 0.5f);
             float y = Constants.WALL_LENGTH * (_random.Next(0, Map.Height) + 0.5f);
-            float angle = _random.Next(0, (int)(2 * Math.PI / Constants.TURN_SPEED)) * Constants.TURN_SPEED;
+            float angle = _random.Next(
+                0, (int)(2 * Math.PI / Constants.MAXIMUM_TURN_SPEED)
+            ) * Constants.MAXIMUM_TURN_SPEED;
             player.PlayerPosition = new Position(x, y, angle);
 
             _logger.Information($"Player {player.ID} spawned at ({x:F2}, {y:F2}) with angle {angle:F2} rad.");
