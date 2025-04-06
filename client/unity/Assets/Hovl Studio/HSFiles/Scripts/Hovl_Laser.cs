13fe8871ba60f6fb0e4c45835ba34084e83510a2
forgrabbit <2133603132@qq.com>
2025-04-07 00:22:39 +0800

    Add Laser

---
+++ client/unity/Assets/Hovl Studio/HSFiles/Scripts/Hovl_Laser.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System;
+using UnityEngine;
+
+public class Hovl_Laser : MonoBehaviour
+{
+    public int damageOverTime = 30;
+
+    public GameObject HitEffect;
+    public float HitOffset = 0;
+    public bool useLaserRotation = false;
+
+    public float MaxLength;
+    private LineRenderer Laser;
+
+    public float MainTextureLength = 1f;
+    public float NoiseTextureLength = 1f;
+    private Vector4 Length = new Vector4(1,1,1,1);
+    //private Vector4 LaserSpeed = new Vector4(0, 0, 0, 0); {DISABLED AFTER UPDATE}
+    //private Vector4 LaserStartSpeed; {DISABLED AFTER UPDATE}
+    //One activation per shoot
+    private bool LaserSaver = false;
+    private bool UpdateSaver = false;
+
+    private ParticleSystem[] Effects;
+    private ParticleSystem[] Hit;
+    public Vector3 CustomStartPosition;
+    public Vector3 CustomEndPosition;
+    [Tooltip("启用后使用自定义的起点终点，否则使用原有逻辑")]
+    public bool UseCustomPoints = true;
+    [Header("起始端效果")]
+    public GameObject FlashEffect;
+
+    void Start ()
+    {
+        //Get LineRender and ParticleSystem components from current prefab;  
+        Laser = GetComponent<LineRenderer>();
+        Effects = GetComponentsInChildren<ParticleSystem>();
+        Hit = HitEffect.GetComponentsInChildren<ParticleSystem>();
+        //if (Laser.material.HasProperty("_SpeedMainTexUVNoiseZW")) LaserStartSpeed = Laser.material.GetVector("_SpeedMainTexUVNoiseZW");
+        //Save [1] and [3] textures speed
+        //{ DISABLED AFTER UPDATE}
+        //LaserSpeed = LaserStartSpeed;
+    }
+
+    void Update()
+    {
+        if (Laser == null || UpdateSaver) return;
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = transform.position + transform.forward * MaxLength;
+
+        if (UseCustomPoints)
+        {
+            Debug.Log(CustomStartPosition);
+            Debug.Log(CustomEndPosition);
+            // 将本地坐标转换为世界坐标
+            startPos = transform.parent.TransformPoint(CustomStartPosition);
+            endPos = transform.parent.TransformPoint(CustomEndPosition);
+            Debug.Log(startPos + " " + endPos);
+
+            Laser.SetPosition(0, startPos);
+            Laser.SetPosition(1, endPos);
+
+            if (FlashEffect != null)
+            {
+                FlashEffect.transform.position = startPos;
+                FlashEffect.transform.rotation = Quaternion.LookRotation(endPos - startPos);
+            }
+
+            float distance = Vector3.Distance(startPos, endPos);
+            Length[0] = MainTextureLength * distance;
+            Length[2] = NoiseTextureLength * distance;
+
+            HitEffect.transform.position = endPos;
+            HitEffect.transform.LookAt(endPos + (endPos - startPos).normalized);
+
+            foreach (var ps in Effects) if (!ps.isPlaying) ps.Play();
+            return;
+        }
+        //if (Laser.material.HasProperty("_SpeedMainTexUVNoiseZW")) Laser.material.SetVector("_SpeedMainTexUVNoiseZW", LaserSpeed);
+        //SetVector("_TilingMainTexUVNoiseZW", Length); - old code, _TilingMainTexUVNoiseZW no more exist
+        Laser.material.SetTextureScale("_MainTex", new Vector2(Length[0], Length[1]));                    
+        Laser.material.SetTextureScale("_Noise", new Vector2(Length[2], Length[3]));
+        //To set LineRender position
+        if (Laser != null && UpdateSaver == false)
+        {
+            Laser.SetPosition(0, transform.position);
+            RaycastHit hit; //DELETE THIS IF YOU WANT USE LASERS IN 2D
+            //ADD THIS IF YOU WANNT TO USE LASERS IN 2D: RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, MaxLength);       
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, MaxLength))//CHANGE THIS IF YOU WANT TO USE LASERRS IN 2D: if (hit.collider != null)
+            {
+                //End laser position if collides with object
+                Laser.SetPosition(1, hit.point);
+
+                    HitEffect.transform.position = hit.point + hit.normal * HitOffset;
+                if (useLaserRotation)
+                    HitEffect.transform.rotation = transform.rotation;
+                else
+                    HitEffect.transform.LookAt(hit.point + hit.normal);
+
+                foreach (var AllPs in Effects)
+                {
+                    if (!AllPs.isPlaying) AllPs.Play();
+                }
+                //Texture tiling
+                Length[0] = MainTextureLength * (Vector3.Distance(transform.position, hit.point));
+                Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, hit.point));
+                //Texture speed balancer {DISABLED AFTER UPDATE}
+                //LaserSpeed[0] = (LaserStartSpeed[0] * 4) / (Vector3.Distance(transform.position, hit.point));
+                //LaserSpeed[2] = (LaserStartSpeed[2] * 4) / (Vector3.Distance(transform.position, hit.point));
+                //Destroy(hit.transform.gameObject); // destroy the object hit
+                //hit.collider.SendMessage("SomeMethod"); // example
+                /*if (hit.collider.tag == "Enemy")
+                {
+                    hit.collider.GetComponent<HittedObject>().TakeDamage(damageOverTime * Time.deltaTime);
+                }*/
+            }
+            else
+            {
+                //End laser position if doesn't collide with object
+                var EndPos = transform.position + transform.forward * MaxLength;
+                Laser.SetPosition(1, EndPos);
+                HitEffect.transform.position = EndPos;
+                foreach (var AllPs in Hit)
+                {
+                    if (AllPs.isPlaying) AllPs.Stop();
+                }
+                //Texture tiling
+                Length[0] = MainTextureLength * (Vector3.Distance(transform.position, EndPos));
+                Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, EndPos));
+                //LaserSpeed[0] = (LaserStartSpeed[0] * 4) / (Vector3.Distance(transform.position, EndPos)); {DISABLED AFTER UPDATE}
+                //LaserSpeed[2] = (LaserStartSpeed[2] * 4) / (Vector3.Distance(transform.position, EndPos)); {DISABLED AFTER UPDATE}
+            }
+            //Insurance against the appearance of a laser in the center of coordinates!
+            if (Laser.enabled == false && LaserSaver == false)
+            {
+                LaserSaver = true;
+                Laser.enabled = true;
+            }
+        }  
+    }
+
+    public void DisablePrepare()
+    {
+        if (Laser != null)
+        {
+            Laser.enabled = false;
+        }
+        UpdateSaver = true;
+        //Effects can = null in multiply shooting
+        if (Effects != null)
+        {
+            foreach (var AllPs in Effects)
+            {
+                if (AllPs.isPlaying) AllPs.Stop();
+            }
+        }
+    }
+}
--- client/unity/Assets/Scripts/Model/Laser.cs
+++ client/unity/Assets/Scripts/Model/Laser.cs
@@ -1,30 +1,94 @@
 using QFramework;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace BattleCity
 {
-    public class Laser
+    public class Laser : ICanGetModel
     {
-        public Position startPos { get; set; }
-        public Position endPos { get; set; }
-               
+        public RecordInfo _recordInfo;
+        public int currentTick;
+        public GameObject LaserObject;
+
+        public Vector3 startPos { get; set; }
+        public Vector3 endPos { get; set; }
+
         public Laser(Position start, Position end)
         {
-            startPos = start;
-            endPos = end;
-            
-        }     
-               
+            _recordInfo = this.GetModel<RecordInfo>();
+            currentTick = _recordInfo.BattleTick;
+            GameObject wallController = GameObject.Find("WallController");
 
-        public void RemoveTrap()
-        {
-           //TODO
+            // 加载并实例化激光预制体
+            GameObject LaserPrefab = Resources.Load<GameObject>("Effects/Laser");
+            startPos = new Vector3(
+                (float)(start.X + Constants.WALL_XBIAS),
+                (float)(start.Y + Constants.Y_BIAS + 0.05f),
+                (float)(start.Z + Constants.WALL_ZFIX)
+            );
+            endPos = new Vector3(
+                (float)(end.X + Constants.WALL_XBIAS),
+                (float)(end.Y + Constants.Y_BIAS + 0.05f),
+                (float)(end.Z + Constants.WALL_ZFIX)
+            );
+
+            LaserObject = Object.Instantiate(LaserPrefab, wallController.transform);
+
+            // 设置激光参数
+            // 将世界坐标转换为父物体的本地坐标
+            var hovlLaser = LaserObject.GetComponent<Hovl_Laser>();
+            if (hovlLaser != null)
+            {
+                hovlLaser.CustomStartPosition = startPos;
+                hovlLaser.CustomEndPosition = endPos;
+            }
+            else
+            {
+                Debug.LogError("Hovl_Laser component missing on LaserPrefab!");
+            }
+
+            // 添加激光生命周期控制器
+            LaserObject.AddComponent<LaserLifecycleController>().Init(this);
+
+            // 注册战斗结束事件
+            TypeEventSystem.Global.Register<BattleEndEvent>(e => DestroyLaser());
         }
 
-        
+        public void DestroyLaser()
+        {
+            if (LaserObject != null)
+            {
+                Object.Destroy(LaserObject);
+                LaserObject = null;
+            }
+        }
+
+        public IArchitecture GetArchitecture()
+        {
+            return GameApp.Interface;
+        }
+    }
+
+    // 激光生命周期控制组件
+    public class LaserLifecycleController : MonoBehaviour
+    {
+        private Laser mOwner;
+
+        public void Init(Laser owner)
+        {
+            mOwner = owner;
+            StartCoroutine(CheckExpiration());
+        }
+
+        private IEnumerator CheckExpiration()
+        {
+            // 每帧检查是否超时
+            while (mOwner._recordInfo.BattleTick <= mOwner.currentTick + 8)
+            {
+                yield return null;
+            }
+            mOwner.DestroyLaser();
+        }
     }
 }
